405365adac9442fc54d8f56ce22bc4c1997128e4
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-15 15:45:14 -0400

    quiz runs on click

--- module.js
+++ module.js
@@ -1,4 +1,5 @@
-let myName = prompt('Enter your name');
+function quiz() {
+    let myName = prompt('Enter your name');
 let month = prompt('What month were you born?');
 let food = prompt('What is your favourite food?');
 /*FUNCTIONS allow us to specify when we want to call a certain bundle of functionality by calling its name*/
@@ -6,3 +7,4 @@ let food = prompt('What is your favourite food?');
 console.log('My name is', myName);
 console.log('I was born in', month);
 console.log('My favourite food is', food);
+}
