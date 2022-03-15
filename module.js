bb77683866a1ebc5115b968369b89991419fd9bd
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-15 15:31:39 -0400

    Added module js file

---
+++ module.js
@@ -0,0 +1,8 @@
+let myName = prompt('Enter your name');
+let month = prompt('What month were you born?');
+let food = prompt('What is your favourite food?');
+/*FUNCTIONS allow us to specify when we want to call a certain bundle of functionality by calling its name*/
+
+console.log('My name is', myName);
+console.log('I was born in', month);
+console.log('My favourite food is', food);
