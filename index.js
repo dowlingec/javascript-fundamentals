798dee46c4fc7631617deb9159824c30c13f3310
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-16 17:44:07 -0400

    array and functions work EOD

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-console.log('JavaScript is running!')
-let score = '100';
+const multiply = (n1, n2) => {
+    console.log("Multiplying", n1, "by", n2)
+    return n1 * n2
+}
 
-// alert(score2);
-alert(score);
+const double = (n) => {
+    return n * 2
+}
+
+let array1 = ['a', 'b', 'c', 'd', 'e']
+let array2 = [1, 2, 3, 4, 5, 6, 7]
+
+const determineLength = (arr) => {
+    if (arr.length > 5) {
+        return 'This array is long'
+    } else {
+        return 'This array is short'
+    }
+}
+
+const askForString = () => {
+    let str = prompt('ENTER A STRING')
+    return  str
+}
+
+console.log(askForString().split(''))
+
+//n is placeholding for an argument, in this case the function
+//computer reads call to double() function, then looks inside
+//ONE RETURN PER FUNCTION
+
+
+// console.log('JavaScript is running!')
+// let score = '100';
+
+// // alert(score2);
+// alert(score);
