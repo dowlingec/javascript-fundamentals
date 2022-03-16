f41d2c54ccb5d75c1eb2c06de7d1efc4ccfb9a21
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-16 12:09:58 -0400

    functions

--- module.js
+++ module.js
@@ -1,10 +1,39 @@
-function quiz() {
+const quiz = () => {
     let myName = prompt('Enter your name');
-let month = prompt('What month were you born?');
-let food = prompt('What is your favourite food?');
-/*FUNCTIONS allow us to specify when we want to call a certain bundle of functionality by calling its name*/
-
-console.log('My name is', myName);
-console.log('I was born in', month);
-console.log('My favourite food is', food);
+    let month = prompt('What month were you born?');
+    let food = prompt('What is your favourite food?');
+    console.log('My name is', myName);
+    console.log('I was born in', month);
+    console.log('My favourite food is', food);
 }
+
+const print = () => {
+    console.log('I love to print text!')
+}
+
+const functionA = (f) => {
+    console.log('FUNCTION A HAS BEEN CALLED')
+    f()
+}
+
+const functionB = () => {
+    console.log('FUNCTION B HAS BEEN CALLED')
+}
+
+const functionC = () => {
+    console.log('FUNCTION C HAS BEEN CALLED!')
+}
+
+const greet = (person) => {
+    console.log('HELLO,', person)
+}
+
+const blender = (string) => {
+    return string.split('')
+}
+
+const square = (num, message) => {
+    console.log('Message:', message)
+    return num * num
+}
+
