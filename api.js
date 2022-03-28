0e30e2197b867ff6e96557745ff3ac92fa47c089
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-28 10:30:24 -0400

    cleaning tree

--- api.js
+++ api.js
@@ -3,17 +3,39 @@ let div = document.getElementById('container')
 //locate element we are working with
 
 const request = async () => {
+    //async to create wait
     let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/javascript-fundamentals/main/mock_data.json')
+    // await fetch results
     let res = await req.json()
+    //await .json file
     console.log('Response', res)
+    //check response
     res.forEach((element) => {
         // console.log('Elements', element.vacancies) to test
         let aptImg = document.createElement('img')
+        let buttonEl = document.createElement('button')
+        let h3 = document.createElement('h3')
+        let h4 = document.createElement('h4')
         //new var creating new element to reference later
+        h3.innerText = element.company_name
+        h4.innerText = `Vacancies: ${element.vacancies}`
+        buttonEl.innerText = (`Rent building ${element.id}`)
+        buttonEl.addEventListener('click', () => {
+            let updatedVacancies = --element.vacancies
+            if (updatedVacancies <= (-1)) return alert('No more vacancies in this building');
+            h4.innerText = `vacancies: ${updatedVacancies}`
+        })
+        //test event listener with alert to see its working
         aptImg.setAttribute('src', element.photo)
-        //set attribute to new element
+        
+        //set attributes to new element
         div.appendChild(aptImg)
-        //append newly created element to div container
+        div.appendChild(h3)
+        div.appendChild(h4)
+        div.appendChild(buttonEl)
+        //append newly created elements to div container - DO THIS IN ORDER YOU WANT TO SEE
+        //alternatively div.append(aptImg, buttonEl, h3)
+
     })
 }
 request()
